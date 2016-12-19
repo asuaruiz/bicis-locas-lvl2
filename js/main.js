62c345bdcbb0a607d41db58d248bdac641cd3c66
andrea <asuaruiz@gmail.com>
2016-12-19 19:17:09 -0300

    falta solo validacion del select

--- js/main.js
+++ js/main.js
@@ -21,43 +21,63 @@ function validateForm(){
 	var apellidoPadre = document.querySelector('.lastname-container');
 	var apellidoError= document.createElement('span');
 	if(apellido===null||apellido.length==0 || /^\s+$/.test(apellido)){
-		notie.alert(3, "Ingresar apellido es obligatorio", 2.5);
+		var textError = document.createTextNode("Ingresar un apellido es obligatorio");
+		apellidoError.appendChild(textError);
+		apellidoPadre.appendChild(apellidoError);
 		return false;
 	}else if(apellido.charAt(0)!== apellido.charAt(0).toUpperCase()){
-		notie.alert(3, "La primera letra del apellido debe ser mayuscula", 2.5);
+		var textError = document.createTextNode("La primera letra debe ser mayuscula");
+		apellidoError.appendChild(textError);
+		apellidoPadre.appendChild(apellidoError);
 		return false;
 	}
 	//email
 	var formatoValido = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 	var email =document.getElementById("input-email").value;
+	var emailPadre = document.querySelector('.email-container');
+	var emailError= document.createElement('span');
 	if(email===null || email.length == 0 || /^\s+$/.test(email)){
-		notie.alert(3, "Ingresar correo es obligatorio", 2.5);
+		var textError = document.createTextNode("Debes ingresar un email");
+		emailError.appendChild(textError);
+		emailPadre.appendChild(emailError);
 		return false;
 	}
 
 	//password
 	var pass= document.getElementById("input-password").value;
+	var passwordPadre = document.getElementById('input-password').parentNode;
+	var passwordError= document.createElement('span');
 	if (pass==null || pass.length == 0 || /^\s+$/.test(email)) {
-		notie.alert(3, "Debes ingresar una Contraseña", 2.5);
+		var textError = document.createTextNode("Debes ingresar una contraseña");
+		passwordError.appendChild(textError);
+		passwordPadre.appendChild(passwordError);
 		return false;
 	}
 	else if (pass.length < 6) {
-		notie.alert(3, "Tu contraseña debe tener al menos 6 caractéres", 2.5);
+		var textError = document.createTextNode("La contraseña debe contener mas de 6 caractéres");
+		passwordError.appendChild(textError);
+		passwordPadre.appendChild(passwordError);
 		return false;
 	}
 	else if (pass === "password" || pass === "123456" || pass === "098754"){
-		notie.alert(3, "La contraseña no puede ser igual a password, 123456 o 098754", 2.5);
+		var textError = document.createTextNode("La contraseña no debe ser igual a password, 123456 o 098754");
+		passwordError.appendChild(textError);
+		passwordPadre.appendChild(passwordError);
 		return false;
 	}
 
 	//select
-	var indice = document.getElementsByTagName('select');
+	var indice = document.getElementsByTagName('select').value;
+	var indicePadre = document.getElementById('select').parentNode;
+	var indiceError= document.createElement('span');
 	/*if( select == null || select == 0 ) {
 		notie.alert(3, "Debes seleccionar una bicicleta", 2.5);
   		return false;*/
   	for (var i = 0; i < indice.length; ++i){ 
 			if(indice[i].value === "0"){
-				notie.alert(3, "Debes seleccionar una bicicleta", 2.5);
+				var textError = document.createTextNode("Debes seleccionar una bicicleta");
+				indiceError.appendChild(textError);
+				indicePadre.appendChild(indiceError);
 				return false;
   		}
   	}
